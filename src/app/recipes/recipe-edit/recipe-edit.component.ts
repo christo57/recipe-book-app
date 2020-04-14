1528eb231b83f127db6e82aa07d5c91661b8f227
Christopher Klein <klein.c@sfeir.com>
2020-04-14 13:10:51 +0200

    end basic forms

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { NgForm, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,6 +10,25 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id: number;
   editMode = false;
+
+  // basic form
+  // @ViewChild('f') signUpForm: FormGroup;
+  // defaultQuestion = 'pet';
+  // answer = '';
+  // genders = ['male', 'female'];
+  // user = {
+  //   username: '',
+  //   email: '',
+  //   secretQuestion: '',
+  //   answer: '',
+  //   gender: '',
+  // };
+  // submitted = false;
+
+  //reactive form
+  signUpForm: FormGroup;
+  genders = ['male', 'female'];
+
 
   constructor(private route: ActivatedRoute) { }
 
@@ -20,5 +40,32 @@ export class RecipeEditComponent implements OnInit {
       }
     );
   }
+
+  //basic form methods
+
+  // suggestUserName() {
+  //   const suggestedName = 'Superuser';
+
+  //   this.signUpForm.form.patchValue({
+  //     userData: {
+  //       username: suggestedName
+  //     }
+  //   });
+  // }
+
+  // // onSubmit(form: NgForm){
+  // //   console.log(form);
+  // // }
+
+  // onSubmit() {
+  //   this.submitted = true;
+  //   this.user.username = this.signUpForm.value.userData.username;
+  //   this.user.email = this.signUpForm.value.userData.email;
+  //   this.user.secretQuestion = this.signUpForm.value.userData.secret;
+  //   this.user.answer = this.signUpForm.value.questionAnswer;
+  //   this.user.gender = this.signUpForm.value.gender;
+
+  //   this.signUpForm.reset();
+  // }
 
 }
